bd269a20dfa4639d06c8a0baf39209c302881c01
jisooround <9utesleben@gmail.com>
2022-11-19 14:13:15 +0900

    221119 - 영화 목록 & 디테일 이미지 리사이징

--- js/renderDetail.js
+++ js/renderDetail.js
@@ -1,14 +1,19 @@
 export default function renderDetail(details) {
   const bgEl = document.getElementById('detail-bg');
   const posterEl = document.getElementById('img-wrap');
+ 
+  // 포스터 이미지 좋은 화질로 바꾸기
+  const imageURL = `${details.Poster}`;
+  const highQualityDetailPoster = imageURL.replace("SX300", "SX700");
+
   document.getElementById('detail-title').innerText = `${details.Title}`;
   document.getElementById('detail-released').innerText = `${details.Released}`;
   document.getElementById('detail-plot').innerText = `${details.Plot}`;
   document.getElementById('actors').innerText = `${details.Actors}`;
   document.getElementById('director').innerText = `${details.Director}`;
   document.getElementById('genre').innerText = `${details.Genre}`;
-  posterEl.style.backgroundImage = `url(${details.Poster})`;
-  bgEl.style.backgroundImage = `url(${details.Poster})`;
+  posterEl.style.backgroundImage = `url(${highQualityDetailPoster})`;
+  bgEl.style.backgroundImage = `url(${highQualityDetailPoster})`;
   
   if(`${details.Poster}` === "N/A"){
     posterEl.style.backgroundImage = `url(https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png)`
@@ -17,11 +22,7 @@ export default function renderDetail(details) {
   const ratingsInfo = document.getElementById('ratings-info');
   ratingsInfo.innerHTML ='';
 
-  function imgHighQuality() {
-    details.Poster.replace('SX300', 'SX700');
-  };
-  imgHighQuality();
-  console.log(details.Poster);
+
 
   // 평점 제공사와 결과 함께 보여주기
   for(let i = 0; i < details.Ratings.length; i++){
@@ -32,7 +33,7 @@ export default function renderDetail(details) {
     const resultEl = document.createElement('p');
     resultEl.className = 'result';
 
-    logoEl.src = '/' + `${details.Ratings[i].Source}` + '.png';
+    logoEl.src = '../images/' + `${details.Ratings[i].Source}` + '.png';
     resultEl.textContent = `${details.Ratings[i].Value}`;
 
     ratingEl.append(logoEl, resultEl);
